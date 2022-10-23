9645df72e0526b8de51c64edf1bc8b4cd8d99be0
rhoctraining <116296783+rhoctraining@users.noreply.github.com>
2022-10-23 16:47:03 +0200

    Initial upload

---
+++ src/main/java/com/parkin/Main.java
@@ -0,0 +1,26 @@
+package com.parkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Main Class
+ */
+@SpringBootApplication
+public class Main extends SpringBootServletInitializer {
+
+    /***
+     *  main class of the application, starts spring context
+     *  @param args xD
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Main.class);
+    }    
+}
